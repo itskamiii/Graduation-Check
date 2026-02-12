1ebd687fb1b048aed5db8180e5308edf2eac3a7f
kami <angjandell24@gmail.com>
2026-02-12 13:49:27 +0800

    Added a For Loop to check a list of students

--- graduation_check.py
+++ graduation_check.py
@@ -1,3 +1,4 @@
+## Credit check for graduation
 name = input("Enter Student Name: ")
 credits = int(input("Enter Credits Completed: "))
 
@@ -6,3 +7,14 @@ if credits >= 120:
 else:
     remaining = 120 - credits
     print(name + " needs " + str(remaining) + " more credits.")
+    
+##credit check but with list
+
+student_credits = [125, 98, 112, 130, 85]
+
+for credits in student_credits:
+    if credits >= 120:
+        print(f"Student with {credits} is eligible to graduate!")
+    else:
+        remaining = 120 - credits
+        print(f"Student with {credits} needs {remaining} more credits.")
